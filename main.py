32661e1bea6a6d741b0d783d52e93fee9b80db78
Nikhil-Yadav97 <ny610630@gmail.com>
2024-11-03 23:56:08 +0530

    added wish function

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 # module for speach recongnition
 import pyttsx3
+import datetime
 
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
 # selecting voice 
 engine.setProperty('voice',voices[0].id)
+
+def wish():
+    hour=int(datetime.datetime.now().hour)
+    if(hour>=0 and hour <12):
+        speak("good morning")
+    elif(hour>=12 and hour<10):
+        speak("good afternoon")
+    else:
+        speak("good evening")
+
 
 # function for speaking the audio given to it 
 def speak(audio):
